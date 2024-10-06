37f376748534d8d969eeb534c457b03c90879d12
Marcin Kosobudzki <marcin.kosobudzki@willhaben.at>
2024-10-07 00:08:11 +0200

    Third homework forlder created - Image Classifier

---
+++ 03-CustomModel/ImageClassifier/ImageClassifier/ImageClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageClassifierApp.swift
+//  ImageClassifier
+//
+//  Created by Marcin Kosobudzki on 07.10.24.
+//
+
+import SwiftUI
+
+@main
+struct ImageClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
