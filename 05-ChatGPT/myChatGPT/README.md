87a8615f3f2b84e12d3e696b6f73b68a763cca2b
Marcin Kosobudzki <marcin.kosobudzki@willhaben.at>
2024-10-26 10:42:55 +0200

    Initial commit

---
+++ 05-ChatGPT/myChatGPT/README.md
@@ -0,0 +1,8 @@
+#  myChatGPT
+
+Kodeco AI Bootcamp, 5th homework, ChatGPT integration into your own applications.
+
+WARNING:
+Please note that this app needs to be run on real iPhone device with minimum version of iOS 18.0.
+
+Video walkthrough:
---
+++ 05-ChatGPT/myChatGPT/myChatGPT/myChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  myChatGPTApp.swift
+//  myChatGPT
+//
+//  Created by Marcin Kosobudzki on 26.10.24.
+//
+
+import SwiftUI
+
+@main
+struct myChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
